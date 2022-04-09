9b43a96206831fda3596dfbb9ac5109eaf22dc2f
unknown <emelgarejox@gmail.com>
2022-04-09 01:00:14 -0500

    filter with lowercase

--- src/controllers/companyController.js
+++ src/controllers/companyController.js
@@ -1,4 +1,4 @@
-const companies = require('../DB/list-companies');
+const providers = require('../DB/list-companies');
 
 exports.getCompanies = (req, res) => {
     const query = req.query.name;
@@ -6,8 +6,19 @@ exports.getCompanies = (req, res) => {
     console.log(req.query.name)
     console.log(req.query.page)
 
-    if(!query) res.json({message: 'Falta parametros'})
+    if(!query) {
+        res.json({message: 'Falta parametros'})
+    }
 
-    const filtered = companies.filter(x => x.name.includes(query));
-    res.json(filtered);
+    const filtered = providers.filter(x => x.name.toLocaleLowerCase().includes(query.toLocaleLowerCase()));
+
+    const response = {
+        companies: [...filtered],
+        totalPages: 4
+    }
+    res.json(response);
+}
+
+exports.getServices = (req, res) => {
+    
 }
